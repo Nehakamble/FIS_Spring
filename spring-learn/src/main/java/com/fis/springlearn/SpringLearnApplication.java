94a559c64cd4715193781e05aebc643b52b0c059
Nehakamble <juhikamble2@gmail.com>
2021-08-10 14:25:52 +0530

    added contry scope

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,13 +1,69 @@
 package com.fis.springlearn;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class SpringLearnApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+//		displayDate();
+//		displayCountry();
+//		displayCountry();
+		displayCountries();
 
 }
+	static void displayCountries() {
+			LOGGER.info("START");
+			ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+			ArrayList<Country> countries = context.getBean("countryList", ArrayList.class);
+			LOGGER.debug("Countries : {}", countries);
+			LOGGER.info("END");
+		}
+
+		static void displayCountry() {
+			ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+			Country country = context.getBean("country", Country.class);
+			Country anotherCountry = context.getBean("country", Country.class);
+			LOGGER.debug("Country : {}", country);
+			LOGGER.debug("Country : {}", anotherCountry);
+		}
+
+	static void displayDate() {
+		LOGGER.info("START");
+		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		 try {
+			Date parseDate = format.parse("31/12/2018");
+	//		System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+	   
+	 SimpleDateFormat format1 = context.getBean("dateFormat1", SimpleDateFormat.class);
+	 try {
+			Date parseDate1 = format1.parse("08/09/2018");
+		//	System.out.println(parseDate1);
+			LOGGER.debug(parseDate1.toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		 LOGGER.info("END");
+	}
+}
+
